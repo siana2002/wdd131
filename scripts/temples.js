52a53062737e4bdf35dec15a72eec0e0a48751c5
sian2002 <arthur.tangi02@gmail.com>
2026-07-11 08:59:14 +1000

    W02 First Commit

---
+++ scripts/temples.js
@@ -0,0 +1,11 @@
+const currentYear = new Date().getFullYear();
+document.getElementById("currentYear").textContent = currentYear;
+document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
+
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+hamButton.addEventListener("click", () => {
+	navigation.classList.toggle("open");
+	hamButton.classList.toggle("open");
+});
---
+++ week02/scripts/constructs-activity.js
@@ -0,0 +1,28 @@
+const DAYS = 6;
+const LIMIT = 30;
+let studentReport = [11, 42, 64, 29, 37, 44];
+
+for (let i = 0; i < studentReport.length; i++) {
+    if (studentReport[i] < LIMIT) {
+        console.log(studentReport[i]);
+    }
+}
+
+let i = 0;
+while (i < studentReport.length) {
+    if (studentReport[i] < LIMIT) {
+        console.log(studentReport[i]);
+    }
+}
+
+studentReport.forEach(function(studentReport) {
+    if (studentReport < LIMIT) {
+        console.log(studentReport);
+    }
+});
+
+for (let i in studentReport) {
+    if (studentReport[i] < LIMIT) {
+        console.log(studentReport[i]);
+    }
+}
