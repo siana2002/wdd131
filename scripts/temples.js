ee6ebef73d01fd5a0a6149cc3169192455df4d71
sian2002 <arthur.tangi02@gmail.com>
2026-07-11 10:03:02 +1000

    W02 7th Commit

--- scripts/temples.js
+++ scripts/temples.js
@@ -4,8 +4,10 @@ document.getElementById("lastModified").textContent = `Last Modified: ${document
 
 const hamButton = document.querySelector("#menu");
 const navigation = document.querySelector(".navigation");
+const title = document.querySelector(".title");
 
 hamButton.addEventListener("click", () => {
 	navigation.classList.toggle("open");
 	hamButton.classList.toggle("open");
+	title.classList.toggle("hide");
 });
